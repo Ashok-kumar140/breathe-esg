bdf2eeaca6e2bdf36c8aea261b4eb224bcb7cf2a
Ashok-kumar140 <ashok.1@iitj.ac.in>
2024-06-07 04:50:32 +0530

    url updated commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://breathe-esg-qjgm.onrender.com/api/v1";
 
 export const userEndPoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
